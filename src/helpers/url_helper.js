bd26d4af27764cfa273b83fa1733fe37a2873e68
putriulhaqq <punyaulhaq@gmail.com>
2024-07-12 10:22:44 +0700

    fix: remove slash

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
     BASE_URL = "http://127.0.0.1:5000"
   } else {
     console.log('not local')
-    BASE_URL = "https://flask-hello-world-one-rosy.vercel.app/"
+    BASE_URL = "https://flask-hello-world-one-rosy.vercel.app"
   }
   
 // Dashboard
